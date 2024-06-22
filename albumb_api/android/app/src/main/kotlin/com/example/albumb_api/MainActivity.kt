a987951d92724955d7133056b753844a982221a4
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2024-06-21 23:14:10 -0700

    Add files via upload

---
+++ albumb_api/android/app/src/main/kotlin/com/example/albumb_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.albumb_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ complex_api/android/app/src/main/kotlin/com/example/complex_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.complex_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
